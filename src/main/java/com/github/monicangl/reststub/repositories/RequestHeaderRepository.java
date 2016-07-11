525af170c01a36c11fbb725c9ec6990f62323aa0
Guiling <monica_n88@163.com>
2016-07-11 16:12:15 +0800

    refactor class RequestHeaderRepository and RequestParameterRepository, delete method findBySchemaId and deleteBySchemaId of them

--- src/main/java/com/github/monicangl/reststub/repositories/RequestHeaderRepository.java
+++ src/main/java/com/github/monicangl/reststub/repositories/RequestHeaderRepository.java
@@ -3,9 +3,5 @@ package com.github.monicangl.reststub.repositories;
 import com.github.monicangl.reststub.models.RequestHeader;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Set;
-
 public interface RequestHeaderRepository extends JpaRepository<RequestHeader, Long> {
-    Set<RequestHeader> findBySchemaId(Long id);
-    void deleteBySchemaId(Long id);
 }
--- src/main/java/com/github/monicangl/reststub/repositories/RequestParameterRepository.java
+++ src/main/java/com/github/monicangl/reststub/repositories/RequestParameterRepository.java
@@ -3,10 +3,6 @@ package com.github.monicangl.reststub.repositories;
 import com.github.monicangl.reststub.models.RequestParameter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Set;
-
 public interface RequestParameterRepository extends JpaRepository<RequestParameter, Long> {
-    Set<RequestParameter> findBySchemaId(Long id);
-    void deleteBySchemaId(Long id);
 }
 
